26ab5d64dfa9989737e2ae77bb6e781edbdf3668
Jeetendra Achtani <jeetendra.achtani@spec-india.com>
2019-08-27 12:18:35 +0530

    Added TestMobile column to Contacts table

--- Tables/dbo.Contacts.sql
+++ Tables/dbo.Contacts.sql
@@ -15,7 +15,8 @@ CREATE TABLE [dbo].[Contacts]
 [TestDateOfBirth] [datetime] NULL,
 [TestPhoneNumber] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [TestAddress] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[TestBottleNumber] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[TestBottleNumber] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[TestMOBILE`] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Contacts] ADD CONSTRAINT [PK_Contacts] PRIMARY KEY CLUSTERED  ([ID]) ON [PRIMARY]

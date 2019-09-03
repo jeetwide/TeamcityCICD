6d832d7025dbfb01e71fd8eaabd956193f2fa59b
Jeetendra Achtani <jeetendra.achtani@spec-india.com>
2019-09-03 12:56:13 +0530

    Added Country currency column

--- Tables/dbo.CountryCodes.sql
+++ Tables/dbo.CountryCodes.sql
@@ -1,7 +1,9 @@
 CREATE TABLE [dbo].[CountryCodes]
 (
 [CountryName] [nvarchar] (255) COLLATE Latin1_General_CI_AS NULL,
-[CountryCode] [nvarchar] (255) COLLATE Latin1_General_CI_AS NOT NULL
+[CountryCode] [nvarchar] (255) COLLATE Latin1_General_CI_AS NOT NULL,
+[CountryLanguage] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CountryCurrency] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[CountryCodes] ADD CONSTRAINT [PK_Countries] PRIMARY KEY CLUSTERED  ([CountryCode]) ON [PRIMARY]

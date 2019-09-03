a4899bc61fddfd5a31094ac0dcc18160acf06eee
Jeetendra Achtani <jeetendra.achtani@spec-india.com>
2019-09-03 15:55:22 +0530

    Added CountryFlags column to countrycodes table

--- Tables/dbo.CountryCodes.sql
+++ Tables/dbo.CountryCodes.sql
@@ -3,7 +3,8 @@ CREATE TABLE [dbo].[CountryCodes]
 [CountryName] [nvarchar] (255) COLLATE Latin1_General_CI_AS NULL,
 [CountryCode] [nvarchar] (255) COLLATE Latin1_General_CI_AS NOT NULL,
 [CountryLanguage] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[CountryCurrency] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[CountryCurrency] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[CountryFlag] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[CountryCodes] ADD CONSTRAINT [PK_Countries] PRIMARY KEY CLUSTERED  ([CountryCode]) ON [PRIMARY]

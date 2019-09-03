00fa574dfaf053af70d4417bffc971052ebcf26d
Jeetendra Achtani <jeetendra.achtani@spec-india.com>
2019-09-03 12:39:33 +0530

    deleted column country language

--- Tables/dbo.CountryCodes.sql
+++ Tables/dbo.CountryCodes.sql
@@ -1,8 +1,7 @@
 CREATE TABLE [dbo].[CountryCodes]
 (
 [CountryName] [nvarchar] (255) COLLATE Latin1_General_CI_AS NULL,
-[CountryCode] [nvarchar] (255) COLLATE Latin1_General_CI_AS NOT NULL,
-[CountryLanguage] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[CountryCode] [nvarchar] (255) COLLATE Latin1_General_CI_AS NOT NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[CountryCodes] ADD CONSTRAINT [PK_Countries] PRIMARY KEY CLUSTERED  ([CountryCode]) ON [PRIMARY]

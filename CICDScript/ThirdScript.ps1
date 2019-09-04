96993c80c02b75342a9b35ad1e5ed1ae10b27d93
Jeetendra Achtani <jeetendra.achtani@spec-india.com>
2019-09-04 17:15:26 +0530

    Create ThirdScript.ps1

---
+++ CICDScript/ThirdScript.ps1
@@ -0,0 +1,8 @@
+﻿$scriptsFolder = $ENV:WORKSPACE
+$packageID = "WidgetPackage"
+$packageVersion = "0.$ENV:BUILD_NUMBER"
+$outputDir = "C:\JenkinsDrops"
+$nupkgpath= $outputDir+'\'+$packageID+'.'+$packageVersion+'.nupkg'
+$validatedScriptsFolder = Invoke-DatabaseBuild $scriptsFolder
+$package = New-DatabaseBuildArtifact $validatedScriptsFolder -PackageId $packageID -PackageVersion $packageVersion
+Export-DatabaseBuildArtifact $package -Path $outputDir
